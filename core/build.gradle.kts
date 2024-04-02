76717200636322f65cfe7d3fa196c8cc64c6667b
vldf <vladislavf7@gmail.com>
2024-04-02 20:23:13 +0300

    Set up JS/TS build

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,15 +1,41 @@
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     kotlin("plugin.serialization") version "1.9.22"
+    id("dev.petuska.npm.publish") version "3.4.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://dl.bintray.com/kotlin/kotlinx/")
+}
+
+rootProject.plugins.withType<YarnPlugin>().configureEach {
+    rootProject.extensions.getByType<YarnRootExtension>().lockFileName = "yarn.lock"
 }
 
 kotlin {
     jvm()
-    js()
+    js(IR) {
+        generateTypeScriptDefinitions()
+        nodejs()
+
+        binaries.library()
+    }
 
     sourceSets {
         commonMain.dependencies {
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+        }
+        jsMain.dependencies {
+            implementation(npm("format-util", "1.0.5", generateExternals = true))
+        }
+        all {
+            languageSettings {
+                optIn("kotlin.js.ExperimentalJsExport")
+            }
         }
     }
 }
