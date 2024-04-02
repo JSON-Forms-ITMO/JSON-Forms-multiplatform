33c84ecd55759d168fd991855cd2159c1fc990aa
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-02 14:14:05 +0300

    delete old parser

--- core/src/commonMain/kotlin/ru/itmo/json_forms/core/SchemaPrinterMain.kt
+++ core/src/commonMain/kotlin/ru/itmo/json_forms/core/SchemaPrinterMain.kt
@@ -11,22 +11,4 @@ fun main() {
     val rawSchema = Json.decodeFromString<JsonObject>(rawJson)
     val schema = Schema(rawSchema)
     println(schema)
-    val schemaParser = JsonSchemaParser(rawSchema)
-    println(schemaParser.dump(rawSchema))
-}
-
-private fun JsonSchemaParser.dump(schema: JsonObject): String = buildString {
-    appendLine("Title: ${getPropertyTitle(schema)}")
-    appendLine("Description: ${getPropertyDescription(schema)}")
-
-    getProperties(schema).forEach { (name, obj) ->
-        appendLine("$name: ${getPropertyType(obj)}")
-        appendWithIndent(dump(obj))
-    }
-}
-
-private fun StringBuilder.appendWithIndent(str: String) {
-    str.lines().forEach { line ->
-        appendLine("  $line")
-    }
 }
--- core/src/commonMain/kotlin/ru/itmo/json_forms/core/schema/Constants.kt
+++
@@ -1,5 +0,0 @@
-package ru.itmo.json_forms.core.schema
-
-object Constants {
-    const val JSON_KEY = "\$"
-}
