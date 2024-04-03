9dc6d576bebb9269e70f9ae17c10b388973bfc45
vldf <vladislavf7@gmail.com>
2024-04-03 19:58:18 +0300

    Complete JsonNumber

--- core/src/commonMain/kotlin/ru/itmo/json_forms/core/document/JsonNumber.kt
+++ core/src/commonMain/kotlin/ru/itmo/json_forms/core/document/JsonNumber.kt
@@ -2,12 +2,12 @@ package ru.itmo.json_forms.core.document
 
 // needed to write a number to json without quotes
 internal class JsonNumber(private val v: String?) : Number() {
-    override fun toByte() = throw NotImplementedError()
-    override fun toDouble() = throw NotImplementedError()
-    override fun toFloat() = throw NotImplementedError()
-    override fun toInt() = throw NotImplementedError()
-    override fun toLong() = throw NotImplementedError()
-    override fun toShort() = throw NotImplementedError()
+    override fun toByte() = v?.toByte() ?: 0
+    override fun toDouble() = v?.toDouble() ?: 0.0
+    override fun toFloat() = v?.toFloat() ?: 0.0f
+    override fun toInt() = v?.toInt() ?: 0
+    override fun toLong() = v?.toLong() ?: 0L
+    override fun toShort() = v?.toShort() ?: 0
 
     override fun toString() = v.toString()
 }
