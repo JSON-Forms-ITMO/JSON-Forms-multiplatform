600f44f35c4860181492c40e6ebca131228c69c6
Aleksei Trifonov <avrong@avrong.me>
2024-04-04 01:28:01 +0300

    Implement UI elements in UI builder

--- intellij-plugin/src/main/kotlin/ru/itmo/hson_forms/intellij/JsonEditorComponent.kt
+++ intellij-plugin/src/main/kotlin/ru/itmo/hson_forms/intellij/JsonEditorComponent.kt
@@ -1,8 +1,12 @@
 package ru.itmo.hson_forms.intellij
 
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.psi.PsiFile
+import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.panel
+import com.jetbrains.rd.util.remove
 import ru.itmo.json_forms.core.document.*
 import javax.swing.JComponent
 import javax.swing.JPanel
@@ -30,54 +34,64 @@ class JsonEditorComponent(
 
     class UiBuilder : DocumentVisitor<JComponent> {
         override fun visitNull(element: NullElement): JComponent {
-            return JBLabel("<number-element> ${element.value}")
+            return JBLabel("<null-element> ${element.value}")
         }
 
-        override fun visitString(element: StringElement): JComponent {
-            return JBLabel("<number-element> ${element.value}")
-        }
+        override fun visitString(element: StringElement): JComponent =
+            JBTextField(element.value)
 
-        override fun visitNumber(element: NumberElement): JComponent {
-            return JBLabel("<number-element> ${element.value}")
-        }
+        override fun visitNumber(element: NumberElement): JComponent =
+            JBTextField(element.value)
 
-        override fun visitInteger(element: IntegerElement): JComponent {
-            return JBLabel("<integer-element> ${element.value}")
-        }
+        override fun visitInteger(element: IntegerElement): JComponent =
+            JBTextField(element.value)
 
         override fun visitBoolean(element: BooleanElement): JComponent {
-            return JBLabel("<bool-element> ${element.value}")
+            val value = element.value == "true"
+            return JBCheckBox(element.type.title, value)
         }
 
-        override fun visitEnum(element: EnumElement): JComponent {
-            return JBLabel("<enum-element> ${element.value}")
-        }
+        override fun visitEnum(element: EnumElement): JComponent =
+            ComboBox(element.type.values)
 
         override fun visitObject(element: ObjectElement): JComponent {
             return panel {
-                row {
-                    label("Object")
-                }
+                val title = element.type.title
 
-                for ((lbl, property) in element.getProperties()) {
-                    row(lbl) {
-                        this.cell(visit(property))
+                group(title, false) {
+                    for ((lbl, property) in element.getProperties()) {
+                        row(lbl) {
+                            this.cell(visit(property))
+                                .comment(property.type.description)
+                        }
                     }
                 }
             }
         }
 
-        override fun visitArray(element: ArrayElement): JComponent {
-            return JBLabel("<array-element>")
+        // TODO: Write or find an editable a-like combobox element
+        override fun visitArray(element: ArrayElement): JComponent = panel {
+            group {
+                row {
+                    button("+") { }
+                }
+
+                for (item in element.items()) {
+                    row {
+                        this.cell(visit(item))
+                        button("-") { element.items().remove(item) }
+                    }
+                }
+            }
         }
 
         override fun visitOptional(element: OptionalElement): JComponent {
-            return JBLabel("<optional-element> ${element.get()}")
+            val trueElement = element.get() ?: return JBLabel("<optional-element> ${element.get()}")
+            return visit(trueElement)
         }
 
         override fun visitUnresolved(element: UnresolvedElement): JComponent {
             return JBLabel("<unresolved-element> ${element.type}")
         }
-
     }
 }
