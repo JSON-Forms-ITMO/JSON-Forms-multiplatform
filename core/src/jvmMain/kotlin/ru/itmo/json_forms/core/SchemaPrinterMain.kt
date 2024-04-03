15f8306bf335e843435e49b8fd68b5aa86c6ff57
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-04-03 10:41:57 +0300

    json parse to `Element`

--- core/src/jvmMain/kotlin/ru/itmo/json_forms/core/SchemaPrinterMain.kt
+++ core/src/jvmMain/kotlin/ru/itmo/json_forms/core/SchemaPrinterMain.kt
@@ -2,16 +2,23 @@ package ru.itmo.json_forms.core
 
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
+import ru.itmo.json_forms.core.element.fromJson
 import ru.itmo.json_forms.core.ir.IrBuilder
 import ru.itmo.json_forms.core.schema.*
 
 fun main() {
     val repoDir = FileUtils.cwd().removeSuffix("/core")
-    val jsonPath = "$repoDir/testdata/aiproj-1.2.json"
+    val jsonPath = "$repoDir/testdata/omnisharp.json"
     val rawJson = FileUtils.readFile(jsonPath)
     val rawSchema = Json.decodeFromString<JsonObject>(rawJson)
     val schema = Schema(rawSchema)
     println(schema)
+
+    val examplePath = jsonPath.replaceFirst(".json", "_example.json")
+    val rawExample = FileUtils.readFile(examplePath)
+    val rawJsonFile = Json.decodeFromString<JsonObject>(rawExample)
+    val root = fromJson(rawJsonFile, schema.root)
+    println(root)
 
     val irBuilder = IrBuilder(schema)
     irBuilder.build("{}")
