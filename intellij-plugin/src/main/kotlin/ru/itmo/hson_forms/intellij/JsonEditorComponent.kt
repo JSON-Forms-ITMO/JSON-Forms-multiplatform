c0f61ab4f06393ff7c087b8c810d7ba7e8e5b679
vldf <vladislavf7@gmail.com>
2024-04-03 22:37:57 +0300

    Semi-done JsonEditorComponent and JsonEditorComponent

--- intellij-plugin/src/main/kotlin/ru/itmo/hson_forms/intellij/JsonEditorComponent.kt
+++ intellij-plugin/src/main/kotlin/ru/itmo/hson_forms/intellij/JsonEditorComponent.kt
@@ -1,10 +1,83 @@
 package ru.itmo.hson_forms.intellij
 
+import com.intellij.psi.PsiFile
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.dsl.builder.panel
+import ru.itmo.json_forms.core.document.*
+import javax.swing.JComponent
 import javax.swing.JPanel
 
-class JsonEditorComponent : JPanel() {
+class JsonEditorComponent(
+    private val file: PsiFile,
+    private val schema: String,
+) : JPanel() {
     init {
-        add(JBLabel("hello!"))
+        updateUi()
+    }
+
+    fun updateUi() {
+        val component = try {
+            val json = file.text
+            val jsonTree = JsonTreeBuilder.build(json, schema)
+            UiBuilder().visitDocument(jsonTree)
+        } catch (_: Exception) {
+            return
+        }
+
+        removeAll()
+        add(component)
+    }
+
+    class UiBuilder : DocumentVisitor<JComponent> {
+        override fun visitNull(element: NullElement): JComponent {
+            return JBLabel("<number-element> ${element.value}")
+        }
+
+        override fun visitString(element: StringElement): JComponent {
+            return JBLabel("<number-element> ${element.value}")
+        }
+
+        override fun visitNumber(element: NumberElement): JComponent {
+            return JBLabel("<number-element> ${element.value}")
+        }
+
+        override fun visitInteger(element: IntegerElement): JComponent {
+            return JBLabel("<integer-element> ${element.value}")
+        }
+
+        override fun visitBoolean(element: BooleanElement): JComponent {
+            return JBLabel("<bool-element> ${element.value}")
+        }
+
+        override fun visitEnum(element: EnumElement): JComponent {
+            return JBLabel("<enum-element> ${element.value}")
+        }
+
+        override fun visitObject(element: ObjectElement): JComponent {
+            return panel {
+                row {
+                    label("Object")
+                }
+
+                for ((lbl, property) in element.getProperties()) {
+                    row(lbl) {
+                        this.cell(visit(property))
+                    }
+                }
+            }
+        }
+
+        override fun visitArray(element: ArrayElement): JComponent {
+            return JBLabel("<array-element>")
+        }
+
+        override fun visitOptional(element: OptionalElement): JComponent {
+            return JBLabel("<optional-element> ${element.get()}")
+        }
+
+        override fun visitUnresolved(element: UnresolvedElement): JComponent {
+            return JBLabel("<unresolved-element> ${element.type}")
+        }
+
     }
 }
--- intellij-plugin/src/main/kotlin/ru/itmo/hson_forms/intellij/JsonEditorProvider.kt
+++ intellij-plugin/src/main/kotlin/ru/itmo/hson_forms/intellij/JsonEditorProvider.kt
@@ -1,27 +1,44 @@
 package ru.itmo.hson_forms.intellij
 
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.EditorKind
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.fileEditor.*
 import com.intellij.openapi.fileEditor.impl.JComponentFileEditor
 import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.impl.http.HttpVirtualFile
+import com.intellij.psi.PsiManager
+import com.jetbrains.jsonSchema.ide.JsonSchemaService
+import java.io.BufferedInputStream
+import java.net.URL
 
 class JsonEditorProvider : FileEditorProvider, DumbAware {
     override fun accept(project: Project, file: VirtualFile): Boolean {
-        return file.extension == "json"
+        if (file.extension != "json") {
+            return false
+        }
+
+        val jsonSchemaService = JsonSchemaService.Impl.get(project)
+
+        if (!jsonSchemaService.isApplicableToFile(file)) {
+            return false
+        }
+
+        val schemaFiles = jsonSchemaService.getSchemaFilesForFile(file)
+        return schemaFiles.isNotEmpty()
     }
 
     override fun createEditor(project: Project, file: VirtualFile): FileEditor {
-        val jsonEditorPanel = JsonEditorComponent()
-
         val editorFactory = EditorFactory.getInstance()
         val fileDocumentManager = FileDocumentManager.getInstance()
         val jsonDocument = fileDocumentManager.getDocument(file) ?: error("can't get document")
 
-        val jsonEditor = editorFactory.createEditor(
+        val allocatedEditor = editorFactory.createEditor(
             jsonDocument,
             project,
             file,
@@ -30,9 +47,40 @@ class JsonEditorProvider : FileEditorProvider, DumbAware {
         )
 
         val textEditorProvider = TextEditorProvider.getInstance()
-        val jsonTextEditor = textEditorProvider.getTextEditor(jsonEditor)
+        val jsonTextEditor = textEditorProvider.getTextEditor(allocatedEditor)
+
+        val psiManager = PsiManager.getInstance(project)
+        val psiFile = psiManager.findFile(file)
+
+        val jsonSchemaContent = getJsonSchemaText(file, project)
+
+        val jsonEditorPanel = JsonEditorComponent(psiFile!!, jsonSchemaContent)
+
+        val jsonModificationListener = object : DocumentListener {
+            override fun documentChanged(event: DocumentEvent) {
+                jsonEditorPanel.updateUi()
+            }
+        }
+
+        jsonDocument.addDocumentListener(jsonModificationListener)
 
         return TextEditorWithPreview(jsonTextEditor, JComponentFileEditor(file, jsonEditorPanel))
+    }
+
+    private fun getJsonSchemaText(file: VirtualFile, project: Project): String {
+        val fileDocumentManager = FileDocumentManager.getInstance()
+
+        val jsonSchemaService = JsonSchemaService.Impl.get(project)
+        val schemaFiles = jsonSchemaService.getSchemaFilesForFile(file)
+        val schemaVirtualFile = schemaFiles.first()
+
+        if (schemaVirtualFile is HttpVirtualFile) {
+            val inp = BufferedInputStream(URL(schemaVirtualFile.url).openStream())
+            return inp.readAllBytes().decodeToString()
+        }
+
+        val schemaDocument = fileDocumentManager.getDocument(schemaVirtualFile)!!
+        return schemaDocument.text
     }
 
     override fun getEditorTypeId(): String {
