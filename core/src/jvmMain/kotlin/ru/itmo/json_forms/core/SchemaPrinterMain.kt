d32020c79f8edee486773608a18e5027354ca92b
vldf <vladislavf7@gmail.com>
2024-04-04 18:23:55 +0300

    Minor

--- core/src/jvmMain/kotlin/ru/itmo/json_forms/core/SchemaPrinterMain.kt
+++ core/src/jvmMain/kotlin/ru/itmo/json_forms/core/SchemaPrinterMain.kt
@@ -1,10 +1,9 @@
 package ru.itmo.json_forms.core
 
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import ru.itmo.json_forms.core.document.Document
-import ru.itmo.json_forms.core.schema.*
+import ru.itmo.json_forms.core.schema.Schema
 
 fun main() {
     val repoDir = FileUtils.cwd().removeSuffix("/core")
@@ -18,9 +17,5 @@ fun main() {
     val rawDocumentText = FileUtils.readFile(documentPath)
     val rawDocumentJson = Json.decodeFromString<JsonObject>(rawDocumentText)
     val document = Document(schema, rawDocumentJson)
-
-    val prettyJson = Json { // this returns the JsonBuilder
-        prettyPrint = true
-    }
-    println(prettyJson.encodeToString(document.getJson()))
+    println(document.getJson())
 }
